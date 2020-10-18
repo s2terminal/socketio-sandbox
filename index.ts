04733332d19ac7a8082f68263ff1026b7b9114f4
s2terminal <suzuki.sh@s2terminal.com>
2020-10-18 13:47:05 +0900

    Integrating Socket.IO

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, () => {
